a34e41a66ecc26630ded99ad0c31d1738e149131
Robert Krahn <robert.krahn@gmail.com>
2015-02-16 14:40:08 -0800

    nrepl handler requires session middleware

--- src/main/rksm/cloxp_repl/nrepl.clj
+++ src/main/rksm/cloxp_repl/nrepl.clj
@@ -7,62 +7,14 @@
         [clojure.tools.nrepl.middleware :as middleware :only (set-descriptor!)])
   (:import clojure.tools.nrepl.transport.Transport))
 
-(def default-bindings {"clojure.core/*print-length*" nil
-                       "clojure.core/*file*" nil})
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+; printing values
 
 (defmacro with-string-writer
   [sym body]
   `(let [~sym (java.io.StringWriter.)]
      ~body
      (str ~sym)))
-
-; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-; extended eval options
-
-(defn prepare-eval
-  [{:keys [op session bindings required-ns code transport] :as msg}]
-  (try
-    (if required-ns
-      (doseq [ns required-ns]
-        (require (symbol ns))))
-    (if bindings
-      (doseq [[name expr] (merge default-bindings (apply hash-map bindings))]
-        (swap! session assoc (find-var (symbol name)) (eval expr))))
-    (catch Exception e
-      (do
-        (t/send transport
-                (response-for msg
-                              :status #{:error :done}
-                              :error (str e)))
-        (throw e))))
-  (assoc msg
-         :op "eval"
-         :code code))
-
-(defn wrap-cloxp-eval
-  ""
-  [h]
-  (fn [{:keys [op] :as msg}]
-    (if (not= op "cloxp-eval")
-      (h msg)
-      (h (prepare-eval msg)))))
-
-(set-descriptor! #'wrap-cloxp-eval
-  {:requires #{}
-   :expects #{"eval"}
-   :handles {"cloxp-eval"
-             {:doc "Evaluation for the cloxp development environment. Allows to set additional options to change the evaluation environment."
-              :requires {"code" "Source string of what should be evaluated."}
-              :optional {"required-ns" "names of namespaces that needs to be loaded for evaluating `code`"
-                         "bindings" "map, keys: names of dynamic vars, vals: expressions. thread-local bindings for evaluating `code`"}
-              :returns (-> (meta #'eval/interruptible-eval)
-                         ::clojure.tools.nrepl.middleware/descriptor
-                         :handles
-                         (get "eval")
-                         :returns)}}})
-
-; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-; printing values
 
 (defn pretty-values
   "a replacement for clojure.tools.nrepl.middleware.pr-values/pr-values that can pretty print"
@@ -100,3 +52,52 @@
                                    (disj #'clojure.tools.nrepl.middleware.pr-values/pr-values)
                                    (conj #'rksm.cloxp-repl.nrepl/pretty-values)))))]
   (set-descriptor! #'eval/interruptible-eval eval-descr))
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+; extended eval options
+
+(def default-bindings {"clojure.core/*print-length*" nil
+                       "clojure.core/*file*" nil})
+
+(defn prepare-eval
+  [{:keys [op session bindings required-ns code transport] :as msg}]
+  (try
+    (if required-ns
+      (doseq [ns required-ns]
+        (require (symbol ns))))
+    (if bindings
+      (doseq [[name expr] (merge default-bindings (apply hash-map bindings))]
+        (swap! session assoc (find-var (symbol name)) (eval expr))))
+    (catch Exception e
+      (do
+        (t/send transport
+                (response-for msg
+                              :status #{:error :done}
+                              :error (str e)))
+        (throw e))))
+  (assoc msg
+         :op "eval"
+         :code code))
+
+(defn wrap-cloxp-eval
+  ""
+  [h]
+  (fn [{:keys [op] :as msg}]
+    (if (not= op "cloxp-eval")
+      (h msg)
+      (h (prepare-eval msg)))))
+
+(set-descriptor! #'wrap-cloxp-eval
+  {:requires #{"clone" "close" #'pretty-values}
+   :expects #{"eval"}
+   :handles {"cloxp-eval"
+             {:doc "Evaluation for the cloxp development environment. Allows to set additional options to change the evaluation environment."
+              :requires {"code" "Source string of what should be evaluated."}
+              :optional {"required-ns" "names of namespaces that needs to be loaded for evaluating `code`"
+                         "bindings" "map, keys: names of dynamic vars, vals: expressions. thread-local bindings for evaluating `code`"}
+              :returns (-> (meta #'eval/interruptible-eval)
+                         ::clojure.tools.nrepl.middleware/descriptor
+                         :handles
+                         (get "eval")
+                         :returns)}}})
+
