3b0a0dfd89b47ceaa45c9d11526505122a856899
Robert Krahn <robert.krahn@gmail.com>
2015-03-13 05:40:05 -0700

    live-eval-code-with-changes, live-eval-code-keeping-env

--- src/main/rksm/cloxp_repl/live_eval.clj
+++ src/main/rksm/cloxp_repl/live_eval.clj
@@ -1,12 +1,44 @@
 (ns rksm.cloxp-repl.live-eval
   (:require [clojure.string :as s]
+            [clojure.set :refer [difference union]]
             [rksm.cloxp-source-reader.core :as src-rdr]))
+
+(defn mapply [f & args]
+  (apply f (apply concat (butlast args) (last args))))
 
 (defn truncate
   [s len]
   (if (> (count s) len)
     (str (.substring s 0 len) "...")
     s))
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+(defonce ^{:doc "cache used by live-eval-code-keeping-env"} envs (atom {}))
+
+(defonce ^{:private true, :doc "Used by do-debounced"} reset-timeouts (ref {}))
+
+(defn do-debounced
+  "Will run func after timeout ms has passed, only if the do-debounced wasn't
+  called inbetween."
+  [id timeout func]
+  (let [now (System/currentTimeMillis)
+        stop-time (+ timeout now)
+        last-entry (get @reset-timeouts id)]
+    (if (or (not last-entry) (< last-entry stop-time))
+      (dosync (alter reset-timeouts assoc id stop-time)))
+    (if-not last-entry ; start thread
+      (future
+       (loop [time now stop-time stop-time]
+         (let [sleep (- stop-time time)]
+           (if (> sleep 0) (Thread/sleep sleep)))
+         (let [now (System/currentTimeMillis)
+               stop-time (get @reset-timeouts id)]
+           (if (< now stop-time)
+             (recur now stop-time)
+             (do
+               (dosync (alter reset-timeouts dissoc id))
+               (func)))))))))
 
 ; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -30,17 +62,83 @@
   (pr-str x))
 
 (defn eval-code
-  [{:keys [form line column]}]
+  [{:keys [form line column source]}]
   (let [s (java.io.StringWriter.)
         value (process-result
                (binding [*out* s]
                  (try (eval form) (catch Exception e e))))
         out (str s)]
-    {:pos {:line line :column column}, :value value, :out out}))
+    {:pos {:line line :column column},
+     :value value, :out out, :source source,
+     :form form, :def? (src-rdr/def? form)}))
+
+(defn prev-def-result
+  [{form :form, :as expr} {:keys [defs], :as env}]
+  (get defs (src-rdr/name-of-def form)))
+
+(defn changed?
+  [{new-src :source, :as expr} env]
+  (let [{old-source :source} (prev-def-result expr env)]
+    (not= new-src old-source)))
+
+(defn make-env
+  [eval-result]
+  {:results (map #(dissoc % :form) eval-result)
+   :defs (apply hash-map
+           (->> eval-result
+             (filter :def?)
+             (mapcat (juxt (comp src-rdr/name-of-def :form) identity))))})
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+; API
 
 (defn live-eval-code
-  [code & {:keys [file ns], :as opts}]
-  (binding [*ns* (if ns (find-ns ns) *ns*) *file* file]
-    (->> (src-rdr/read-objs code)
-      (map eval-code)
-      doall)))
+  "Takes source code and evaluates the top-level statements in it one-by-one.
+  Will return a map {:results [{:value :pos :def? :source}], :defs {defnames -> result}}"
+  [code & {:keys [file ns env], :as opts}]
+  (binding [*ns* (if ns (find-ns ns) *ns*)
+            *file* file]
+    (let [eval-result
+          (map (fn [{:keys [form line column] :as expr}]
+                 (if (or (not (src-rdr/def? form)) (changed? expr env))
+                   (eval-code expr)
+                   (assoc (prev-def-result expr env)
+                          :pos {:line line :column column})))
+               (src-rdr/read-objs code))
+          changed-env (make-env eval-result)]
+      changed-env)))
+
+(defn live-eval-code-with-changes
+  "Will compare source of defs in env with defs that are defined in code. Will
+  only eval those defs for which the source has changed. This avoids
+  unnecessarily re-defining defs and the side effects this has."
+  [code & {:keys [env ns] :or {ns 'user} :as opts}]
+  (let [{old-defs :defs} env
+        {defs :defs, :as eval-result} (mapply live-eval-code code opts)
+        added (difference (set (keys defs)) (set (keys old-defs)))
+        removed (difference (set (keys old-defs)) (set (keys defs)))
+        changed (set (filter
+                      (fn [def-name]
+                        (let [get-val (fn [defs] (-> defs (get def-name) :value))]
+                          (not= (get-val defs) (get-val old-defs))))
+                      (difference (set (keys defs)) added)))
+        changes {:added added, :removed removed, :changed changed}]
+    (doseq [def-name removed] (ns-unmap ns def-name))
+    (assoc eval-result :changes changes)))
+
+(defn live-eval-code-keeping-env
+  "Use this version of eval to have the live-eval state maintained for a
+  certain amount of time (reset-timeout). After the timeout passed the state is
+  cleared."
+  [code & {:keys [id reset-timeout] :as opts}]
+  {:pre [(not (nil? id))]
+   :post [(contains? @envs id)]}
+  (let [prev-env (get @envs id)
+        eval-result (mapply
+                     live-eval-code-with-changes
+                     code (assoc opts :env prev-env))]
+    (swap! envs assoc id eval-result)
+    (when reset-timeout
+      (do-debounced
+       id reset-timeout #(swap! envs dissoc id)))
+    eval-result))
--- src/test/rksm/cloxp_repl/live_eval_test.clj
+++ src/test/rksm/cloxp_repl/live_eval_test.clj
@@ -6,27 +6,83 @@
   
   (testing "eval statements"
    (let [code "(def x 23)\n\n(+ x 2)\n3"
-         eval-result (live-eval-code code :ns 'user)]
+         {:keys [defs results]} (live-eval-code code :ns 'user)]
      (is (= [{:pos {:line 1, :column 1}, :out "",
-              :value "x => 23"}
-             {:pos {:line 3, :column 1}, :out "", :value "25"}
-             {:pos {:line 4, :column 1}, :out "", :value "3"}]
-            eval-result))))
+              :source "(def x 23)" :value "x => 23", :def? true}
+             {:pos {:line 3, :column 1}, :out "", :value "25", :source "(+ x 2)", :def? false}
+             {:pos {:line 4, :column 1}, :out "", :value "3", :source "3", :def? false}]
+            results))
+     (is (= ['x] (keys defs)))))
 
   (testing "with errors"
     (let [code "(/ 1 0)"
-         eval-result (live-eval-code code :ns 'user)]
+         {:keys [defs results]} (live-eval-code code :ns 'user)]
      (is (= [{:pos {:line 1, :column 1}, :out "",
+              :source "(/ 1 0)", :def? false,
               :value "#<ArithmeticException java.lang.ArithmeticException: Divide by zero>"}]
-            eval-result))))
+            results))))
   
   (testing "stdout"
     (let [code "(pr 123)"
-         eval-result (live-eval-code code :ns 'user)]
+         {:keys [defs results]} (live-eval-code code :ns 'user)]
      (is (= [{:pos {:line 1, :column 1}, :out "123",
-              :value "nil"}]
-            eval-result))))
-  )
+              :source "(pr 123)", :def? false, :value "nil"}]
+            results)))))
+
+(deftest generates-def-changes
+  (let [tests [{:code "(def x 23)\n\n(+ x 2)\n3"
+                :expected {:added #{'x}, :removed #{}, :changed #{}}}
+               {:code "(def x 23)\n\n(+ x 2)\n3"
+                :expected {:added #{}, :removed #{}, :changed #{}}}
+               {:code "(def x 24)\n\n(+ x 2)\n3"
+                :expected {:added #{}, :removed #{}, :changed #{'x}}}
+               {:code "\n(+ x 2)\n3"
+                :expected {:added #{}, :removed #{'x}, :changed #{}}}]]
+    (loop [def-state {} tests tests]
+      (when-not (empty? tests)
+        (let [[{:keys [code expected]} & rest] tests
+              {:keys [changes results] :as new-state}
+              (live-eval-code-with-changes code :ns 'user :env def-state)]
+          (is (= expected changes) code)
+          (recur new-state rest))))))
+
+(deftest disappearing-defs-are-removed
+  (let [tests [{:code "(def x 23)"
+                :expected {:added #{'x}, :removed #{}, :changed #{}}}
+               {:code "3"
+                :expected {:added #{}, :removed #{'x}, :changed #{}}}]]
+    (loop [def-state {} tests tests]
+      (when-not (empty? tests)
+        (let [[{:keys [code expected]} & rest] tests
+              {:keys [changes results] :as new-state}
+              (live-eval-code-with-changes code :ns 'user :env def-state)]
+          (is (= expected changes) code)
+          (recur new-state rest))))
+    (is (nil? (-> 'user ns-interns (get 'x))))))
+
+(deftest keeping-changes-test
+  (let [tests [{:code "(def x 23)\n\n(+ x 2)\n3"
+                :expected {:added #{'x}, :removed #{}, :changed #{}}}
+               {:code "(def x 23)\n\n(+ x 2)\n3"
+                :expected {:added #{}, :removed #{}, :changed #{}}}
+               {:code "(def x 24)\n\n(+ x 2)\n3"
+                :expected {:added #{}, :removed #{}, :changed #{'x}}}
+               {:code "\n(+ x 2)\n3"
+                :expected {:added #{}, :removed #{'x}, :changed #{}}}]]
+    (doseq [t tests]
+      (let [{:keys [code expected]} t
+            {:keys [changes results]} (live-eval-code-keeping-env
+                                       code
+                                       :ns 'user
+                                       :id 'keeping-changes-test
+                                       :reset-timeout 100)]
+        (is (= expected changes))
+        (Thread/sleep 50)))
+    (is (not (nil? (get @envs 'keeping-changes-test))))
+    (Thread/sleep 110)
+    (is (nil? (get @envs 'keeping-changes-test)))))
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 (comment
  (run-tests *ns*)
