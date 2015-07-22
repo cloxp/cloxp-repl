4710d97bb2d27a59bc36fcc4c23d7b3e64f4eb1d
Robert Krahn <robert.krahn@gmail.com>
2015-07-22 14:41:22 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-repl "0.1.7"
+(defproject org.rksm/cloxp-repl "0.1.8-SNAPSHOT"
   :description "Source reading, parsing, and querying for cloxp."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
@@ -7,7 +7,7 @@
                  [org.clojure/tools.nrepl "0.2.10"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [clj-stacktrace/clj-stacktrace "0.2.8"]
-                 [org.rksm/cloxp-source-reader "0.1.8"]]
+                 [org.rksm/cloxp-source-reader "0.1.9-SNAPSHOT"]]
   :source-paths ["src/main"]
   :test-paths ["src/test"]
   :scm {:url "git@github.com:cloxp/cloxp-repl.git"}
